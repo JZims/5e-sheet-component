e9607cf3dca9450f74155664f929efd86620a6e1
JZims <zimmerman.j@hotmail.com>
2023-10-04 00:35:07 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+
+
+
+function App() {
+ 
+
+
+  return (
+   
+    <div className="container " id="main-sheet"> 
+      <div className="flex flex-row" id="header-section">
+        <div className="basis-1/2 px-4"id="character-name">
+          Name Here
+        </div>
+        <div className="basis-1/2 px-4"id="basic-char-info">
+          Info Here
+        </div>
+      </div>
+    </div>
+  );
+};
+  
+
+export default App
+
+
+  {/* <div className="character-sheet">
+  
+      <div className="input-group">
+        <label htmlFor="character-name" > Character Name:</label>
+        <input type="text" id="character-name" />
+      </div>
+
+      <div className="input-group">
+        <label htmlFor="character-class">Character Class:</label>
+        <input type="text" id="character-class" />
+      </div>
+
+      <div className="input-group">
+        <label htmlFor="character-level"> Character Level:</label>
+        <input type="text" id="character-level" />
+      </div>
+      </div> */}
